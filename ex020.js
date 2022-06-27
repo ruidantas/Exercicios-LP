6da2220a0650c4d8a8c66389cfc3930e0b86d984
ruidantas <ruifisicauesb@hotmail.com>
2022-06-27 00:14:51 -0300

    ex-20

---
+++ ex020.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")()
+console.clear()
+
+while (true){
+console.log("Cadastre-se no site")
+    let nome = prompt("Digite um nome de usuario: ")
+let senha = prompt("Digite uma senha: ")
+
+
+if (nome == senha)
+console.log("ERRO")
+
+
+break
+} 
+
+
